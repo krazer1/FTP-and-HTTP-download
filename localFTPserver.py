a0cf03c090f08e76f5464a84c4c05709d9c80557
krazer1 <134173429+krazer1@users.noreply.github.com>
2023-11-24 19:51:28 +0700

    Create localFTPserver.py

---
+++ localFTPserver.py
@@ -0,0 +1,17 @@
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+def run_ftp_server():
+    authorizer = DummyAuthorizer()
+    authorizer.add_user("ivan", "ftpserver", ".", perm="elradfmw")
+
+    handler = FTPHandler
+    handler.authorizer = authorizer
+
+    server = FTPServer(("127.0.0.1", 21), handler)
+    server.serve_forever()
+
+if __name__ == "__main__":
+    run_ftp_server()
+
